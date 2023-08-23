315fc75494b55ab092c1c0361d5b9e2748eb4ce1
Esvararajaa <142655605+Esvararajaa@users.noreply.github.com>
2023-08-23 09:16:31 +0530

    Update Remove Vowels.py

--- Remove Vowels.py
+++ Remove Vowels.py
@@ -1,11 +1,15 @@
 def remove_vowels(string1):
     vowels_chars = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
     result = ''
+    # iterate the string
     for i in string1:
+        # compare each character with vowels list
         if i not in vowels_chars:
+            #concatenate non vowels characters
             result = result + i
     print("String without vowels: ", result)
 
 
 str_var = input("Enter the string to remove vowels: ")
-remove_vowels(str_var)
+str_no_vowels = remove_vowels(str_var)
+print("String without vowels: ", str_no_vowels)
